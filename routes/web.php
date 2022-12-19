83c22775e1730e758593a2fe43e898137948e60b
A.J. Krumholz <krumholz.alan@gmail.com>
2022-12-19 14:32:42 -0500

    add edit link to show page

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,6 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 
 Route::post('/listings', [ListingController::class, 'store']);
 
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
