8594413b89cb58181fe5295f1497a7a6a6eee568
A.J. Krumholz <krumholz.alan@gmail.com>
2022-12-19 08:39:21 -0500

    setup html and db from class files

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,29 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-  public static function all() {
-    return [
-      [
-        'id' => 1,
-        'title' => 'Listing One',
-        'description' => 'some text here'
-      ],
-      [
-        'id' => 2,
-        'title' => 'Listing Two',
-        'description' => 'some more text here'
-      ]
-    ];
-  }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-  public static function find($id) {
-    $listings = self::all();
-
-    foreach($listings as $listing) {
-      if($listing['id'] == $id) {
-        return $listing;
-      }
-    }
-  }
+class Listing extends Model
+{
+    use HasFactory;
 }
