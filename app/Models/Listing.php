9e2fb8c58221ce32f6bddfc6e7e177239189621b
A.J. Krumholz <krumholz.alan@gmail.com>
2022-12-20 10:04:34 -0500

    add relationship functions to models

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -23,4 +23,10 @@ class Listing extends Model
         };
     }
     
+    // Relationship to user
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    
 }
